81f979cfc154d0317b2df41965e750699213bfe1
Sausage <wardrox@gmail.com>
2014-08-01 14:59:43 -0400

    can login with GitHub

--- website.js
+++ website.js
@@ -8,28 +8,109 @@ var express = require("express"),
 	ROOT_DIRECTORY = __dirname,
 	ASSETS_DIRECTORY = path.join(ROOT_DIRECTORY, 'public'),
 	flash = require('connect-flash'),
-	passport = require('passport');
+	passport = require('passport'),
+	util = require('util'),
+	GitHubStrategy = require('passport-github').Strategy;
 
-app.use(express.static(ASSETS_DIRECTORY));
-app.use(express.bodyParser());
-app.use(logger('dev'));
-app.use(cookieParser());
-app.use(flash());
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
 
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	store: new MongoStore({
-		url: process.env.MONGO_CONNECTION_STRING
-	})
-}));
+passport.deserializeUser(function (obj, done) {
+	done(null, obj);
+});
 
-app.set('view engine', 'ejs');
+passport.use(new GitHubStrategy({
+		clientID: process.env.GITHUB_CLIENT_ID,
+		clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		callbackURL: process.env.GITHUB_CALLBACK_URL
+	},
+	function (accessToken, refreshToken, profile, done) {
+		// asynchronous verification, for effect...
+		process.nextTick(function () {
 
-app.get('/', function(request, response) {
-	response.send(200);
+			// To keep the example simple, the user's GitHub profile is returned to
+			// represent the logged-in user.  In a typical application, you would want
+			// to associate the GitHub account with a user record in your database,
+			// and return that user instead.
+			return done(null, profile);
+		});
+	}
+));
+
+// configure Express
+app.configure(function () {
+
+	app.use(express.static(ASSETS_DIRECTORY));
+	app.use(logger('dev'));
+	app.use(flash());
+
+	app.use(session({
+		secret: process.env.SESSION_SECRET,
+		store: new MongoStore({
+			url: process.env.MONGO_CONNECTION_STRING
+		})
+	}));
+
+	app.set('view engine', 'ejs');
+
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+
+	// Initialize Passport!  Also use passport.session() middleware, to support
+	// persistent login sessions (recommended).
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		user: req.user
+	});
+});
+
+app.get('/account', ensureAuthenticated, function (req, res) {
+	res.render('account', {
+		user: req.user
+	});
+});
+
+app.get('/login', function (req, res) {
+	res.render('login', {
+		user: req.user
+	});
+});
+
+app.get('/auth/github',
+	passport.authenticate('github'),
+	function (req, res) {
+		// The request will be redirected to GitHub for authentication, so this function will not be called.
+	});
+
+app.get('/auth/github/callback',
+	passport.authenticate('github', {
+		failureRedirect: '/login'
+	}),
+	function (req, res) {
+		res.redirect('/');
+	});
+
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
 	console.log("Listening on " + port);
 });
+
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login')
+}
